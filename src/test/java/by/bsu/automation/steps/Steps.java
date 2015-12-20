b0c587225d840924c21bb2a1dc7174ce183ca4f9
mrahachou <mickolk@gmail.com>
2015-12-20 21:55:42 +0300

    Increased timeouts

--- src/test/java/by/bsu/automation/steps/Steps.java
+++ src/test/java/by/bsu/automation/steps/Steps.java
@@ -21,8 +21,8 @@ public class Steps
     public void initBrowser()
     {
         driver = new FirefoxDriver();
-        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         logger.info("Initialized browser");
     }
 
